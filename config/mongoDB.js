937404b283f5528a3bcf2479b6bfea3009776c1d
Deepthy <deepthy.s@titechnologies.in>
2016-03-29 17:18:16 +0530

    bug fixes

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -5,5 +5,5 @@ var config = require( './index.js' );
 
 
 console.log("=========================");
-console.log(process.env.DATABASE_URL);
-mongoose.connect( process.env.DATABASE_URL );
+console.log(process.env.MONGOHQ_URL);
+mongoose.connect( process.env.MONGOHQ_URL );
